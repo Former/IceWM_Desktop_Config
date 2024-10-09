027e2730e52a2bd5483b4219df10a807d66bf770
Alexei Bezborodov <AlexeiBv+x220@narod.ru>
2024-10-09 17:15:04 +0300

    install

---
+++ install.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cur_dir=$(pwd)
+
+ln -s ${cur_dir}/redshift.conf ~/.config/redshift.conf
+ln -s ${cur_dir}/.screenrc ~/.screenrc
+ln -s ${cur_dir}/.ideskrc ~/.ideskrc
+ln -s ${cur_dir}/.icewm ~/.icewm
+ln -s ${cur_dir}/.idesktop ~/.idesktop
+
+
